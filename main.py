041bfceb1546c3cff103d10a221e8adf51929681
MrManhattann <95130362+MrManhattann@users.noreply.github.com>
2025-05-28 20:54:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+# a=float(input("Fist number:"))
+# s=input("Simbol:")
+# b=int(input("Secon number:"))
+# res=float(a)+float(b)
+# a=float(a)
+# b=float(b)
+# res=a+b
+# if s == "+" :
+#     res=a+b
+# elif s=="-":
+#     res=a-b
+# elif s == "*":
+#     res=a*b
+# elif s == "/":
+#     res=a/b
+# elif s == "//":
+#     res=a//b
+# elif s == "%":
+#     res=a%b
+# else: res="EROR"
+# print(res)
+#Task1
+# number=float(input("Введіть число:"))
+# if number%2==0:
+#     number="Even number"
+# elif number%2:
+#     number="Odd number"
+# print(number)
+#Task2
+# number=float(input("Введіть число:"))
+# if number%7==0:
+#     number="Number is multiple 7"
+# elif number%7:
+#     number="Number is not multiple 7"
+# print(number)
+#Task3
+# num1=float(input("First number:"))
+# num2=float(input("Second number:"))
+# if (num1>num2):
+#     print("Max1",num1)
+# else:
+#     print("Max2", num2)
+#Task4
+# num1=float(input("First number:"))
+# num2=float(input("Second number:"))
+# if (num1<num2):
+#     print("Min1",num1)
+# else:
+#     print("Min2", num2)
+#Task5
+# num1=float(input("Fist number:"))
+# articl=input("Виберіть дію +,-,* або ser:")
+# num2=float(input("Secon number:"))
+# if articl == "+" :
+#     res=num1+num2
+# elif articl=="-":
+#     res=num1-num2
+# elif articl == "*":
+#     res=num1*num2
+# else: res=(num1+num2)/2
+# print(res)
+#Task6
+# num1=float(input("Введіть суму в доларах:"))
+# articl=input("Виберіть валюту на вибір із(EUR(евро),GBP(фунти),JPY(єни)):")
+# num2=float(input("Ведіть поточний курс вибраної валюти:"))
+# if articl==num2:
+#     res=num1*num2
+# print(res)
+
+
+
+
+
+
+
+
+
+
+
